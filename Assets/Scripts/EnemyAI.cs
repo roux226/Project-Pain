014ad3e28689d3d5100708934a8e6c0fdfe992e4
roux226 <thegame22yt22@gmail.com>
2023-03-08 11:05:49 +0100

    Added a really bad enemy AI. Gonna make it better in the future

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    private PlayerMovement playerMovement;
+
+    public Transform playerTransform;
+    public float speed = 7f;
+    public float fovRadius = 5f;
+    public float viewAngle = 45f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        GameObject player = GameObject.Find("Player");
+        playerMovement = player.GetComponent<PlayerMovement>();
+    }
+
+    private void Update()
+{
+    if (playerTransform == null) return; // add null check
+        
+    Vector2 direction = (playerTransform.position - transform.position).normalized;
+
+    // Check if player is within FOV radius
+    float distanceToPlayer = Vector2.Distance(transform.position, playerTransform.position);
+    if (distanceToPlayer <= fovRadius)
+    {
+        // Check if player is within view angle
+        float angleToPlayer = Vector2.Angle(transform.right, direction);
+        if (angleToPlayer <= viewAngle / 2f)
+        {
+            // Move towards player
+            rb.velocity = direction * speed;
+        }
+        else
+        {
+            rb.velocity = Vector2.zero;
+        }
+    }
+    else
+    {
+        rb.velocity = Vector2.zero;
+    }
+}
+
+        void OnCollisionEnter2D(Collision2D collision)
+{
+    if (collision.gameObject.CompareTag("Player"))
+    {
+        PlayerMovement playerMovement = collision.gameObject.GetComponent<PlayerMovement>();
+        if (playerMovement != null && !playerMovement.isDashing)
+        {
+            Destroy(collision.gameObject);
+        }
+
+        else if (playerMovement != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
+
+
+    private void OnDrawGizmosSelected()
+    {
+        // Draw FOV radius
+        Gizmos.color = new Color(1f, 1f, 0f, 0.2f);
+        Gizmos.DrawSphere(transform.position, fovRadius);
+
+        // Draw FOV angle
+        Gizmos.color = new Color(1f, 1f, 0f, 0.4f);
+        Vector3 fovLine1 = Quaternion.AngleAxis(viewAngle / 2f, Vector3.forward) * transform.right * fovRadius;
+        Vector3 fovLine2 = Quaternion.AngleAxis(-viewAngle / 2f, Vector3.forward) * transform.right * fovRadius;
+        Gizmos.DrawLine(transform.position, transform.position + fovLine1);
+        Gizmos.DrawLine(transform.position, transform.position + fovLine2);
+    }
+}
